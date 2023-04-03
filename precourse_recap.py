4ce2cf4ca4e56ed8637dfe2bb18ff809a61acef2
Sam <sdcbartram@icloud.com>
2023-04-03 17:54:16 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,7 @@
+days_reading = 22
+hours_reading_per_day = 2
+total_hours_reading = hours_reading_per_day * days_reading
+
+
+print("This year I have read for a total of " + str(total_hours_reading) + " hours! That means I've read for " +
+      str(hours_reading_per_day) + " hours per day, for " + str(days_reading) + " days!")
