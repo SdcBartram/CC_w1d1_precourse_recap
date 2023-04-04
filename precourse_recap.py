1dd7d989618d9098dc4a4996d62d90bc4592b09c
Sam <sdcbartram@icloud.com>
2023-04-04 07:55:58 +0100

    second commit, comments added

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,7 +1,12 @@
+# variables created of different types
 days_reading = 22
 hours_reading_per_day = 2
 total_hours_reading = hours_reading_per_day * days_reading
 
+# print function called using assigned variables and some operators
+# str function used to convert integer to string
 
 print("This year I have read for a total of " + str(total_hours_reading) + " hours! That means I've read for " +
       str(hours_reading_per_day) + " hours per day, for " + str(days_reading) + " days!")
+
+# output reads: This year I have read for a total of 44 hours! That means I've read for 2 hours per day, for 22 days!
